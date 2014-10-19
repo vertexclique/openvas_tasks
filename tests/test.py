20268d3c06405fbf2486fc99c2d94138346eabf3
Mahmut Bulut <mahmutbulut0@gmail.com>
2014-10-19 18:52:15 +0300

    database path for tests.

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@ import openvas_tasks
 # openvas_tasks.get_all_tasks(dbpath="/home/mahmut/Downloads/tasks.db")
 # openvas_tasks.get_all_tasks()
 # b = openvas_tasks.Pool("/Users/vertexclique/projects/tasks.db")
-b = openvas_tasks.Pool("../tasks.db")
+b = openvas_tasks.Pool("/home/travis/build/vertexclique/openvas_tasks/tasks.db")
 b.tasks_data()
 b.tasks_count()
 b.results_data()
