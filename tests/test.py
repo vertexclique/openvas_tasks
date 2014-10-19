0d11239b971f47ac82bc497ffc375c5dbf431899
Mahmut Bulut <mahmutbulut0@gmail.com>
2014-10-19 18:43:49 +0300

    Shown tasks added, code reformatted, orm mappings of Targets and Reports

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,8 @@
 import openvas_tasks
 # openvas_tasks.get_all_tasks(dbpath="/home/mahmut/Downloads/tasks.db")
 # openvas_tasks.get_all_tasks()
-b = openvas_tasks.Pool("/Users/vertexclique/projects/tasks.db")
+# b = openvas_tasks.Pool("/Users/vertexclique/projects/tasks.db")
+b = openvas_tasks.Pool("../tasks.db")
 b.tasks_data()
 b.tasks_count()
 b.results_data()
